c267a467197c54c8be8081b70176ee8898581a03
Kim Svensson <ks6g10@soton.ac.uk>
2012-02-13 15:01:33 +0000

    Working now with creating file and directory with the username settings line

--- io.c
+++ io.c
@@ -7,8 +7,13 @@ int main()
 	const char SETTINGS_PATH[] = "./.cymplayer/settings";
 	const char DIR_PATH[] = "./.cymplayer";
 	const char READ = 'r';
-	char * operation = &READ;
+	const char WRITE = 'w';
+	const char USERNAME[] = "Username=";
+	unsigned int *  charcnt; 
+	*charcnt = 200;
 	char * path = &DIR_PATH;
+	char * input = (char *) (malloc(sizeof(char)*charcnt));
+	char ** inputptr = &input;
 	FILE *settings;
 	DIR * settings_dir;
 	int ret = 0;
@@ -27,13 +32,24 @@ int main()
 	closedir(settings_dir);
 		
 	path = &SETTINGS_PATH;
-	settings = fopen(path,operation);
-	if(settings == NULL){
+	settings = fopen(path,&READ);
+	if(settings == NULL)
+	{
 		printf("File %s does not exist\n",SETTINGS_PATH);
-		
+		settings = fopen(path,&WRITE);
+		if(settings == NULL) {
+			printf("Could not write file %s\n",SETTINGS_PATH);
+			exit(2);
+		}
+		fprintf(settings,"%s\n",USERNAME);
 
 	}
+	else
+	{
+		getline(inputptr,charcnt,settings);
+	}
 
+	fclose(settings);
 	return 0;
 
 
